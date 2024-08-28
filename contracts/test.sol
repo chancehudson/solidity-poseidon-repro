2651416541afa48acebe2ca15f750435c62ba272
vimwitch <jchancehud@gmail.com>
2024-08-28 12:37:44 -0700

    deploy a fresh copy of poseidont3 to test with

--- contracts/test.sol
+++ contracts/test.sol
@@ -1,16 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.26;
+
+import "./PoseidonT3.sol";
 
 interface PoseidonT2 {
     function hash(uint[1] calldata input) external pure returns (uint);
 }
 
 contract Test {
-    function test() public pure returns (uint) {
-        return
-            PoseidonT2(0x22233340039aAB0C858bc6086f508d9A4f2fA4db).hash(
-                [2 ** 250]
-            );
+    function test() public view returns (uint) {
+        uint a = PoseidonT2(0x22233340039aAB0C858bc6086f508d9A4f2fA4db).hash(
+            [2 ** 250]
+        );
+        uint b = PoseidonT3.hash([2 ** 250, 3]);
+        return a + b;
     }
 }
--- test/a.test.js
+++ test/a.test.js
@@ -2,7 +2,14 @@ const { ethers } = require("hardhat");
 
 describe("asfas", () => {
   it("should execute", async () => {
-    const testFactory = await ethers.getContractFactory("Test", {});
+    const poseidonFactory = await ethers.getContractFactory("PoseidonT3");
+    const PoseidonT3 = await poseidonFactory.deploy();
+    await PoseidonT3.deployed();
+    const testFactory = await ethers.getContractFactory("Test", {
+      libraries: {
+        PoseidonT3: PoseidonT3.address,
+      },
+    });
     const Test = await testFactory.deploy();
     await Test.deployed();
     console.log(Test.address);
